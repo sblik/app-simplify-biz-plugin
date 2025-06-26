99012c36fb31162409ae4a11ab9930c999ed4eb1
TomCom1234 <tomcom12344@gmail.com>
2025-06-26 11:02:03 +0100

    Reinstate repos

---
+++ public/php/repositories/InviteGuestParentRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SMPLFY\appsimplifybiz;
+
+use SmplfyCore\SMPLFY_BaseRepository;
+use SmplfyCore\SMPLFY_GravityFormsApiWrapper;
+use WP_Error;
+
+/**
+ *
+ * @method static InviteGuestParentEntity|null get_one($fieldId, $value)
+ * @method static InviteGuestParentEntity|null get_one_by_id($value)
+ * @method static InviteGuestParentEntity|null get_one_for_current_user()
+ * @method static InviteGuestParentEntity|null get_one_for_user($userId)
+ * @method static InviteGuestParentEntity[] get_all(array $filters = null, string $direction = 'ASC')
+ * @method static InviteGuestParentEntity[] update(InviteGuestParentEntity $entity)
+ * @method static int|WP_Error add(InviteGuestParentEntity $entity)
+ */
+class InviteGuestParentRepository extends SMPLFY_BaseRepository
+{
+    public function __construct(SMPLFY_GravityFormsApiWrapper $gravityFormsApi)
+    {
+        $this->entityType = InviteGuestParentEntity::class;
+        $this->formId     = FormIds::INVITE_GUEST_PARENT;
+
+        parent::__construct($gravityFormsApi);
+    }
+}
