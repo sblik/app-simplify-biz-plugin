e0eefb1256ad09d219f22322528e5f32ba6f98f3
TomCom1234 <tomcom12344@gmail.com>
2025-06-26 11:09:30 +0100

    If overview entity exists, link to single entry view for it in menu item

--- public/php/types/MenuItemIDs.php
+++ public/php/types/MenuItemIDs.php
@@ -4,9 +4,10 @@ namespace SMPLFY\appsimplifybiz;
 
 class MenuItemIDs
 {
-    const START       = 100623;
-    const ADD         = 100631;
-    const VIEW_UPDATE = 100632;
-    const USER        = 408;
-    const SOLOPRENEUR = 565;
+    const START          = 100623;
+    const ADD            = 100631;
+    const VIEW_UPDATE    = 100632;
+    const USER           = 408;
+    const SOLOPRENEUR    = 565;
+    const OVERVIEW_ENTRY = 100640;
 }
--- public/php/usecases/ModifyMenuItems.php
+++ public/php/usecases/ModifyMenuItems.php
@@ -17,14 +17,21 @@ class ModifyMenuItems
     {
         $userID = get_current_user_id();
 
+        $overviewEntity           = $this->overviewRepository->get_one_for_user($userID);
+        $marketingPlansRepository = $this->marketingPlanRepository->get_one_for_user($userID);
+
         foreach ($menu_items as $menu_item) {
 
             if ($menu_item->ID == MenuItemIDs::START) {
-
-                $overviewEntity           = $this->overviewRepository->get_one_for_user($userID);
-                $marketingPlansRepository = $this->marketingPlanRepository->get_one_for_user($userID);
-                
                 if (!empty($overviewEntity) && !empty($marketingPlansRepository)) {
+                    $menu_item->title = '';
+                    $menu_item->url   = '';
+                }
+            }
+            if ($menu_item->ID == MenuItemIDs::OVERVIEW_ENTRY) {
+                if (!empty($overviewEntity)) {
+                    $menu_item->url = SITE_URL . '/view/overview/entry/' . $overviewEntity->id;
+                } else {
                     $menu_item->title = '';
                     $menu_item->url   = '';
                 }
