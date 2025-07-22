e64cc363851240a5fa6212eb404d8fbcb4ba1552
TomCom1234 <tomcom12344@gmail.com>
2025-07-22 15:21:34 +0100

    Remove redundant methods

--- public/php/usecases/Shortcodes.php
+++ public/php/usecases/Shortcodes.php
@@ -18,106 +18,7 @@ class Shortcodes
         $this->tasksRepository            = $tasksRepository;
         $this->processSalesRepository     = $processSalesRepository;
     }
-
-
-    /**
-     * @param $atts
-     * @return string|null
-     */
-    function strategy_link_shortcode($atts)
-    {
-        // Define default attributes and allow overrides
-        $atts = shortcode_atts([
-            'class' => '',
-        ], $atts, 'smplfy_strategy_link_shortcode');
-
-
-        $userID         = get_current_user_id();
-        $strategyEntity = $this->strategyRepository->get_one_for_user($userID);
-
-        $url   = do_shortcode('[gv_entry_link entry_id="' . $strategyEntity->id . '" view_id="' . ViewIDs::STRATEGY . '"]Strategy[/gv_entry_link]');
-        $class = esc_attr($atts['class']);
-
-
-        if (!empty($strategyEntity)) {
-            $viewLink = '';
-            if (preg_match('/href="([^"]+)"/', $url, $matches)) {
-                $viewLink = $matches[1];
-            }
-
-            return "<a href='$viewLink' class='smplfy-heading-link smplfy-bg_strategy'><i class='fa-sharp fa-solid  fa-compass'></i> <h3>Strategy</h3></a>";
-        } else {
-            $url = SITE_URL . '/start/?id=67';
-            return "<a href='$url' class='smplfy-heading-link smplfy-bg_strategy'><i class='fa-sharp fa-solid  fa-compass'></i> <h3>Strategy</h3></a>";
-        }
-    }
-
-    /**
-     * @param $atts
-     * @return string|null
-     */
-    function marketing_plan_link_shortcode($atts)
-    {
-        // Define default attributes and allow overrides
-        $atts = shortcode_atts([
-            'class' => '',
-        ], $atts, 'smplfy_marketing_link_shortcode');
-
-
-        $userID                 = get_current_user_id();
-        $marketingProcessEntity = $this->marketingProcessRepository->get_one_for_user($userID);
-
-        $url   = do_shortcode('[gv_entry_link entry_id="' . $marketingProcessEntity->id . '" view_id="' . ViewIDs::PROCESS_MARKETING . '"]Marketing[/gv_entry_link]');
-        $class = esc_attr($atts['class']);
-
-
-        if (!empty($marketingProcessEntity)) {
-            $viewLink = '';
-            if (preg_match('/href="([^"]+)"/', $url, $matches)) {
-                $viewLink = $matches[1];
-            }
-
-            return "<a href='$viewLink' class='smplfy-heading-link smplfy-bg_marketing'><i class='fa-sharp fa-solid  fa-megaphone'></i> <h3>Marketing</h3></a>";
-        } else {
-            $url = SITE_URL . '/start/?id=80';
-
-            return "<a href='$url' class='smplfy-heading-link smplfy-bg_marketing'><i class='fa-sharp fa-solid  fa-megaphone'></i> <h3>Marketing</h3></a>";
-        }
-    }
-
-    /**
-     * @param $atts
-     * @return string|null
-     */
-    function sales_link_shortcode($atts)
-    {
-        // Define default attributes and allow overrides
-        $atts = shortcode_atts([
-            'class' => '',
-        ], $atts, 'smplfy_sales_link_shortcode');
-
-
-        $userID             = get_current_user_id();
-        $processSalesEntity = $this->processSalesRepository->get_one_for_user($userID);
-
-        $url   = do_shortcode('[gv_entry_link entry_id="' . $processSalesEntity->id . '" view_id="' . ViewIDs::PROCESS_SALES . '"]Sales[/gv_entry_link]');
-        $class = esc_attr($atts['class']);
-
-
-        if (!empty($processSalesEntity)) {
-            $viewLink = '';
-            if (preg_match('/href="([^"]+)"/', $url, $matches)) {
-                $viewLink = $matches[1];
-            }
-
-            return "<a href='$viewLink' class='smplfy-heading-link smplfy-bg_sales'><i class='fa-solid fa-handshake'></i> <h3>Sales</h3></a>";
-        } else {
-            $url = SITE_URL . '/start/?id=91';
-            return "<a href='$url' class='smplfy-heading-link smplfy-bg_sales'><i class='fa-solid fa-handshake'></i> <h3>Sales</h3></a>>";
-        }
-    }
-
-
+    
     /**
      * @param $atts
      * @return string|null
@@ -155,7 +56,7 @@ class Shortcodes
             if ($form == 'marketing') {
                 $entity = $this->marketingProcessRepository->get_one_for_user($userID);
                 $viewID = ViewIDs::PROCESS_MARKETING;
-                $formID = FormIds::MARKETING_PLAN;
+                $formID = FormIds::PROCESS_MARKETING;
             }
             if ($form == 'sales') {
                 $entity = $this->processSalesRepository->get_one_for_user($userID);
@@ -191,7 +92,6 @@ class Shortcodes
             $url = SITE_URL . '/start/?id=' . $formID;
             return "<a href='$url' class='$class'><i class='$fontawesome'></i> <h3>$capitalisedFormName</h3></a>";
         }
-        return $matches;
     }
 
 }
