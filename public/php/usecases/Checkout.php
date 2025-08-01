945fffdcc174e4993cd7940c7c8eaf1af4e12f4e
TomCom1234 <tomcom12344@gmail.com>
2025-08-01 16:05:14 +0100

    Logic to have an organisation field

--- public/php/usecases/Checkout.php
+++ public/php/usecases/Checkout.php
@@ -21,31 +21,13 @@ class Checkout
     public function handle_checkout($fields)
     {
         SMPLFY_Log::info("FIelds before change: ", $fields);
-        $fields['billing']['billing_email']['priority'] = 10;
         // Add a full name field before the email field
-        $fields['billing']['billing_contact_first_name'] = array(
+        $fields['billing']['organisation_name'] = array(
             'type'     => 'text',
-            'label'    => __('First Name', 'woocommerce'),
+            'label'    => __('Organisation Name', 'woocommerce'),
             'required' => true,
-            'priority' => 5, // show before email
             'class'    => array('form-row-wide', 'stripe-gateway-checkout-email-field'),
         );
-        $fields['billing']['billing_contact_last_name']  = array(
-            'type'     => 'text',
-            'label'    => __('Last Name', 'woocommerce'),
-            'required' => true,
-            'priority' => 5, // show before email
-            'class'    => array('form-row-wide', 'stripe-gateway-checkout-email-field'),
-        );
-        $fields['billing']['billing_contact_phone']      = array(
-            'type'     => 'text',
-            'label'    => __('Phone', 'woocommerce'),
-            'required' => true,
-            'priority' => 9, // show before email
-            'class'    => array('form-row-wide', 'stripe-gateway-checkout-email-field'),
-        );
-
-
         return $fields;
     }
 }
